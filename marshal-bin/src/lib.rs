5ce7dea97cea9808d19d9879bba39363c3f89ace
Nathan Dobson <nadobson@mac.com>
2024-06-05 16:56:04 -0700

    Add missing files.

---
+++ marshal-bin/src/lib.rs
@@ -0,0 +1,10 @@
+#![feature(slice_take)]
+#![feature(utf16_extra)]
+#![deny(unused_must_use)]
+#![allow(unused_mut)]
+#![allow(dead_code)]
+#![feature(test)]
+
+mod write;
+mod to_from_vu128;
+mod read;
