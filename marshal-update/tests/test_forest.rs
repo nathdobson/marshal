cdf5602a286246a06de775d6881a405e11c73db3
Nathan Dobson <nadobson@mac.com>
2024-06-27 21:21:29 -0700

    Add disabled test for derive combined with forest.

--- marshal-update/tests/test_forest.rs
+++ marshal-update/tests/test_forest.rs
@@ -6,7 +6,6 @@ use marshal_update::forest::forest::Forest;
 use marshal_update::forest::forest::ForestRoot;
 use marshal_update::forest::forest::Tree;
 use marshal_update::tester::Tester;
-
 #[test]
 fn test_forest() -> anyhow::Result<()> {
     let forest = Forest::new();
@@ -90,3 +89,61 @@ fn test_forest_insert() -> anyhow::Result<()> {
     }
     Ok(())
 }
+
+
+
+// #[test]
+// fn test_forest_list() -> anyhow::Result<()> {
+//     #[derive(Serialize, Deserialize, SerializeUpdate, SerializeStream, DeserializeUpdate)]
+//     struct List {
+//         a: Arc<Tree<u8>>,
+//         b: Option<Arc<Tree<List>>>,
+//     }
+//     let forest = Forest::new();
+//     let tree: Arc<Tree<Option<Arc<Tree<u8>>>>> = forest.add(None);
+//     let root = ForestRoot::new(forest, tree.clone());
+//     let mut tester = Tester::new(
+//         root,
+//         r#"{
+//   "id": 0,
+//   "inner": {
+//     "None": null
+//   }
+// }"#,
+//     )?;
+//     assert!(tester
+//         .output()
+//         .forest()
+//         .get(tester.output().root())
+//         .is_none());
+//     tester.next(
+//         r#"{
+//   "root": null,
+//   "trees": {}
+// }"#,
+//     )?;
+//     assert!(tester
+//         .output()
+//         .forest()
+//         .get(tester.output().root())
+//         .is_none());
+//     *tester.input_mut().forest_mut().get_mut(&tree) = Some(tester.input().forest().add(4));
+//     tester.next(
+//         r#"{
+//   "root": null,
+//   "trees": {
+//     "0": {
+//       "id": 1,
+//       "inner": 4
+//     }
+//   }
+// }"#,
+//     )?;
+//     {
+//         let o = tester.output();
+//         let r = o.root();
+//         let f = o.forest();
+//         assert_eq!(*f.get(f.get(r).as_ref().unwrap()), 4);
+//     }
+//     Ok(())
+// }
